f12f98e43f5f1154803e2a6b49c17635c83390f6
Yosoyret <randyenyer1@gmail.com>
2026-04-19 22:48:22 -0400

    change

--- app.js
+++ app.js
@@ -2,32 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(`
-    <html>
-      <head>
-        <title>Mi App</title>
-        <style>
-          body {
-            background: #113079;
-            color: white;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            font-family: Arial;
-          }
+  res.send('Hola Mundo');
 
-          h1 {
-            color: #38bdf8;
-            font-size: 3rem;
-          }
-        </style>
-      </head>
-      <body>
-        <h1>Segundo cambio de prueba</h1>
-      </body>
-    </html>
-  `);
+ 
 });
 
 module.exports = app;
