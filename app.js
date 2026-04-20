f288fe3a5f81c8b477069955088d236c603f1045
Yosoyret <randyenyer1@gmail.com>
2026-04-19 22:11:50 -0400

    Cambio

--- app.js
+++ app.js
@@ -3,6 +3,33 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hola Mundo');
+
+  res.send(`
+    <html>
+      <head>
+        <title>Mi App</title>
+        <style>
+          body {
+            background: #0f172a;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            font-family: Arial;
+          }
+
+          h1 {
+            color: #38bdf8;
+            font-size: 3rem;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Segundo cambio</h1>
+      </body>
+    </html>
+  `);
 });
 
 module.exports = app;
